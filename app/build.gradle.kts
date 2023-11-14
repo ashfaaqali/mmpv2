1e0671b7dedb43392cd93b407d2ee40d1332f18d
Mohd Ashfaq <tbeast305@gmail.com>
2023-11-14 13:30:50 +0530

    firebase crashlytics and authentication added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -48,4 +50,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-crashlytics")
+    implementation("com.google.firebase:firebase-analytics")
 }
--- app/src/main/java/com/example/motometerprov2/MainActivity.kt
+++ app/src/main/java/com/example/motometerprov2/MainActivity.kt
@@ -2,18 +2,28 @@ package com.example.motometerprov2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.motometerprov2.databinding.ActivityMainBinding
 import com.example.motometerprov2.fragments.MeterFragment
 import com.example.motometerprov2.fragments.SettingsFragment
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.crashlytics.crashlytics
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
 
+        signInAnonymously()
 
         setFragment(MeterFragment())
         // Handle bottom navigation item selection
@@ -38,4 +48,20 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragment_container, fragment)
             .commit()
     }
+
+
+    private fun signInAnonymously(){
+        auth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val userId = auth.currentUser?.uid.toString()
+                    // Sign in success, update UI with the signed-in user's information
+                    Firebase.crashlytics.setUserId(userId)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Firebase Anonymous Auth", "signInAnonymously:failure", task.exception)
+                }
+            }
+    }
+
 }
