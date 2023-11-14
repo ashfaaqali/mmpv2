3643b837c1600febddca1779535f02b074412d01
Mohd Ashfaq <tbeast305@gmail.com>
2023-11-14 13:46:01 +0530

    implement in app updates

--- app/src/main/java/com/example/motometerprov2/MainActivity.kt
+++ app/src/main/java/com/example/motometerprov2/MainActivity.kt
@@ -6,14 +6,24 @@ import androidx.fragment.app.Fragment
 import com.example.motometerprov2.databinding.ActivityMainBinding
 import com.example.motometerprov2.fragments.MeterFragment
 import com.example.motometerprov2.fragments.SettingsFragment
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var appUpdate: AppUpdateManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        appUpdate = AppUpdateManagerFactory.create(this)
 
         setFragment(MeterFragment())
         // Handle bottom navigation item selection
@@ -37,5 +47,57 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragment)
             .commit()
+    }
+
+    private val listener = InstallStateUpdatedListener { state ->
+        if (state.installStatus() == InstallStatus.DOWNLOADED) {
+            appUpdate.completeUpdate()
+        }
+    }
+
+    private fun checkUpdate() {
+        appUpdate.appUpdateInfo.addOnSuccessListener { updateInfo ->
+            if (updateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                if (updateInfo.updatePriority() >= 4 && updateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                    appUpdate.startUpdateFlowForResult(
+                        updateInfo,
+                        AppUpdateType.IMMEDIATE,
+                        this,
+                        9
+                    )
+                } else if (updateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
+                    appUpdate.startUpdateFlowForResult(
+                        updateInfo,
+                        AppUpdateType.FLEXIBLE,
+                        this,
+                        9
+                    )
+                    appUpdate.registerListener(listener)
+                }
+            }
+        }
+    }
+
+    private fun updateInProgress() {
+        appUpdate.appUpdateInfo.addOnSuccessListener { updateInfo ->
+            if (updateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                appUpdate.startUpdateFlowForResult(
+                    updateInfo,
+                    AppUpdateType.IMMEDIATE,
+                    this,
+                    9
+                )
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateInProgress()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        appUpdate.unregisterListener(listener)
     }
 }
